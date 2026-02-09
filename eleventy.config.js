46eb8da49d3c6b657175e86a006dc2a68775e96a
dirmanhana <dirmanhana@gmail.com>
2026-02-10 02:01:44 +0700

    feat: Add Decap CMS integration with admin configuration and entry point.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addPassthroughCopy("src/admin");
 
     eleventyConfig.addFilter("dateReadable", (dateObj) => {
         return dateObj.toLocaleDateString('id-ID', { year: 'numeric', month: 'long', day: 'numeric' });
